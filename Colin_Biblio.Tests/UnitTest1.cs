7f2c65af9e22133861fb7c1a5b9f29b926ad2a7c
Jujuco1996 <Justine_Colin@users.noreply.github.com>
2018-10-16 09:29:48 +0200

    Ajoutez des fichiers projet.

---
+++ Colin_Biblio.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Colin_Biblio.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Colin_Biblio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Colin_Biblio.MvcApplication" Language="C#" %>
